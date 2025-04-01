68ab047cdf7b24f9c11f66285e069640a4d37432
Mia Nieto <mini3149@colorado.edu>
2025-04-01 10:21:29 -0600

    ica/readingquiz

---
+++ readingquiz11/index.js
@@ -0,0 +1,41 @@
+function squared(num) {
+    return num * num;
+  }
+  
+function cubed(num) {
+    return num * num * num;
+  }
+  
+function factorial(num) {
+    if (num < 0) return undefined;
+    if (num === 0) return 1;
+    let x = num - 1;
+    while (x > 1) {
+      num *= x;
+      x--;
+    }
+    return num;}
+
+
+//my added functions
+function fourthPower(num) {
+    return num * num * num * num;
+  }
+
+function devidedByTwo(num) {
+    return (num/2);
+  }
+
+
+//the given printing code
+input.addEventListener("change", () => {
+    const num = parseFloat(input.value);
+    if (isNaN(num)) {
+      para.textContent = "You need to enter a number!";
+    } else {
+      para.textContent = `${num} squared is ${squared(num)}. `;
+      para.textContent += `${num} cubed is ${cubed(num)}. `;
+      para.textContent += `${num} factorial is ${factorial(num)}. `;
+    }
+  });
+
