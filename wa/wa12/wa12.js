d2646c713795d455f91ea2bf7adf904827328820
Mia Nieto <mini3149@colorado.edu>
2025-04-14 22:49:03 -0600

    wa12

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -1,41 +1,74 @@
-var newBtn = document.querySelector('#js-new-quote').addEventListener('click', getCatFact);
-var answerBtn = document.querySelector('#js-tweet').addEventListener('click', getDogFact);
-var factEndpoint = "https://catfact.ninja/fact?max_length=100";
-let current = {
+var catBtn = document.querySelector('#js-cat').addEventListener('click', getFactCat);
+var dogBtn = document.querySelector('#js-dog').addEventListener('click', getFactDog);
+var nameBtn = document.querySelector('#js-name').addEventListener('click', getName);
+var endpointCat = "https://catfact.ninja/fact?max_length=100";
+var endpointDog = "https://dogapi.dog/api/v2/facts?limit=1";
+
+var endpointName = "https://randomuser.me/api?nat=US"
+
+
+
+let currentCat = {
     fact: "",
-    dogFact: "",
+
 };
 
-var dogEndpoint = "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1";
+let currentDog ={
+    fact:"",
+    
+}
+
+let currentName = {
+    name: "",
+}
 
 
-async function getCatFact(){
+async function getFactCat(){
     try{
-        const response = await fetch(factEndpoint);
+        const response = await fetch(endpointCat);
         if(!response.ok){
             throw Error(response.statusText);
         }
         const json = await response.json();
-        displayCat(json.fact);
-        current.fact = json.fact;
+        displayCatFact(json.fact);
+        currentCat.fact = json.fact;
     }
-
     catch(err){
         console.log(err);
-        alert('Fail');
+        alert('Failed to get cat fact');
     }
 }
 
-
-async function getDogFact(){
+async function getFactDog(){
     try{
-        const dogResponse = await fetch(dogEndpoint);
-        if(!dogResponse.ok){
-            throw Error(dogResponse.statusText);
+        const responseDog = await fetch(endpointDog);
+        if(!responseDog.ok){
+            throw Error(responseDog.statusText);
         }
-        const dogJson = await dogResponse.json();
-        displayDog(dogJson[0].fact);
-        current.fact = dogJson[0].fact;
+        const json = await responseDog.json();
+        const fact = json.data[0].attributes.body
+        displayDogFact(fact);
+        currentDog.fact = fact;
+    }
+    catch(err){
+        console.log(err);
+        alert('Failed to get dog fact');
+    }
+}
+
+async function getName(){
+    try{
+        const responseName = await fetch(endpointName);
+        if(!responseName.ok){
+            throw Error(responseName.statusText);
+        }
+        const json = await responseName.json();
+        displayName(json.results[0].name.first);
+        currentName.name = json.results[0].name.first;
+    }
+    catch(err){
+        console.log(err);
+        alert('Failed to get name');
     }
 }
 
@@ -44,15 +77,26 @@ async function getDogFact(){
 
 
 
-function displayCat(quote){
-    
-    const catText = document.querySelector('#js-quote-text');
-    catText.textContent = quote;
+
+
+
+function displayCatFact(catFact){
+    const catText = document.querySelector('#js-cat-text');
+    catText.textContent = catFact;
 }
 
-function displayDog(answer){
-    const breedText = document.querySelector ('#js-answer-text');
-    dogText.textContent = fact;
+function displayDogFact(dogFact){
+    const dogText = document.querySelector ('#js-dog-text');
+    dogText.textContent = dogFact;
 }
 
-getCatFact();
+function displayName(name){
+    const nameText = document.querySelector ('#js-name-text');
+    nameText.textContent = name;
+}
+
+
+getFactCat();
+getFactDog();
+getName();
+
